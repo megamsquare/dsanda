d94b2a4d7e47eed09e341946cacf6187b343619f
Michael Mensah <msquaremega@gmail.com>
2022-09-23 09:30:11 +0100

    add db schema

---
+++ db/migrations/000001_create_books_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS books(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(100),
+    publication VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+);
