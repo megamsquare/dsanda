7767569547ae9c4f7cb5b8c57ec3c8bac29627eb
Michael Mensah <msquaremega@gmail.com>
2022-10-17 16:44:29 +0100

    add time zone to postgres connection

--- config/app.go
+++ config/app.go
@@ -9,7 +9,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "host=localhost user=dsanda password=postgrespass dbname=postgresdb port=5432 sslmode=disable"
+	dsn := "host=localhost user=dsanda password=postgrespass dbname=postgresdb port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database! \n Error: " + err.Error())
