ce93f94ae388591e25dd83ad13c7e8a417ccc149
Michael Mensah <msquaremega@gmail.com>
2022-10-24 13:05:55 +0100

    add migrate to app

--- config/app.go
+++ config/app.go
@@ -4,6 +4,7 @@ import (
 	"gorm.io/gorm"
 	// "github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
+	"github.com/megamsquare/dsanda/db"
 )
 
 var DB *gorm.DB
@@ -11,15 +12,16 @@ var DB *gorm.DB
 func ConnectDB() {
 	// dsn := "host=db user=dsanda password=postgrespass dbname=postgresdb port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	dsn := "postgres://dsanda:postgrespass@db:5432/postgresdb"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dbase, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database! \n Error: " + err.Error())
 	}
-	DB = db
+	DB = dbase
 
+	
 	// Migrate the schema
 	// golang migrate working with gorm
-	
+
 }
 
 func GetDB() *gorm.DB {
